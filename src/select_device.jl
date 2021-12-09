ef548c27004d9edcaa2331bda6a98180e7fc653d
Samuel Omlin <samuel.omlin@cscs.ch>
2021-12-09 19:33:26 +0100

    let CUDA.functional report error in select_device

--- src/select_device.jl
+++ src/select_device.jl
@@ -1,9 +1,7 @@
 export select_device
 
 import MPI
-@static if ENABLE_CUDA
-    using CUDA
-end
+using CUDA
 
 """
     select_device()
@@ -12,14 +10,10 @@ Select the device (GPU) corresponding to the node-local MPI rank and return its 
 """
 function select_device()
     check_initialized();
-    @static if ENABLE_CUDA
-        @assert CUDA.functional(true)
-        comm_l = MPI.Comm_split_type(comm(), MPI.MPI_COMM_TYPE_SHARED, me())
-    	if (MPI.Comm_size(comm_l) > length(CUDA.devices())) error("More processes have been launched per node than there are GPUs available."); end
-    	me_l = MPI.Comm_rank(comm_l)
-        CUDA.device!(me_l)
-        return me_l
-    else
-        error("Cannot select a GPU because ImplicitGlobalGrid was not precompiled for GPU usage (as CUDA was not functional).")
-    end
+    @assert CUDA.functional(true)
+    comm_l = MPI.Comm_split_type(comm(), MPI.MPI_COMM_TYPE_SHARED, me())
+	if (MPI.Comm_size(comm_l) > length(CUDA.devices())) error("More processes have been launched per node than there are GPUs available."); end
+	me_l = MPI.Comm_rank(comm_l)
+    CUDA.device!(me_l)
+    return me_l
 end
