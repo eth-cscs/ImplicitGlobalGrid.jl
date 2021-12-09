ee375d54aa5c348c13de3b119bce9a7243f1ecbf
Samuel Omlin <samuel.omlin@cscs.ch>
2021-12-09 20:19:01 +0100

    Control error message if CUDA was not functional at module load time

--- src/select_device.jl
+++ src/select_device.jl
@@ -9,11 +9,15 @@ using CUDA
 Select the device (GPU) corresponding to the node-local MPI rank and return its ID. This function only needs to be called when using nodes with more than one device.
 """
 function select_device()
-    check_initialized();
-    @assert CUDA.functional(true)
-    comm_l = MPI.Comm_split_type(comm(), MPI.MPI_COMM_TYPE_SHARED, me())
-	if (MPI.Comm_size(comm_l) > length(CUDA.devices())) error("More processes have been launched per node than there are GPUs available."); end
-	me_l = MPI.Comm_rank(comm_l)
-    CUDA.device!(me_l)
-    return me_l
+	if cuda_enabled()
+	    check_initialized();
+	    @assert CUDA.functional(true)
+	    comm_l = MPI.Comm_split_type(comm(), MPI.MPI_COMM_TYPE_SHARED, me())
+		if (MPI.Comm_size(comm_l) > length(CUDA.devices())) error("More processes have been launched per node than there are GPUs available."); end
+		me_l = MPI.Comm_rank(comm_l)
+	    CUDA.device!(me_l)
+	    return me_l
+	else
+		error("Cannot select a device because CUDA was detected not functional when the ImplicitGlobalGrid module was loaded.")
+	end
 end
