43f4337f5f6e98b03c917e5519eeb6d11e816f1a
Samuel Omlin <samuel.omlin@cscs.ch>
2022-02-23 19:27:48 +0100

    Fix select_device

--- test/test_select_device.jl
+++ test/test_select_device.jl
@@ -19,13 +19,13 @@ nprocs = MPI.Comm_size(MPI.COMM_WORLD); # NOTE: these tests can run with any num
 @testset "$(basename(@__FILE__)) (processes: $nprocs)" begin
     @testset "1. select_device" begin
         @static if test_cuda
-            me, = init_global_grid(3, 4, 5; quiet=true, init_MPI=false; device_type="CUDA");
+            me, = init_global_grid(3, 4, 5; quiet=true, init_MPI=false, device_type="CUDA");
             gpu_id = select_device();
             @test gpu_id < length(CUDA.devices())
             finalize_global_grid(finalize_MPI=false);
         end
         @static if test_amdgpu
-            me, = init_global_grid(3, 4, 5; quiet=true, init_MPI=false; device_type="AMDGPU");
+            me, = init_global_grid(3, 4, 5; quiet=true, init_MPI=false, device_type="AMDGPU");
             gpu_id = select_device();
             @test gpu_id < length(AMDGPU.get_agents(:gpu))
             finalize_global_grid(finalize_MPI=false);
