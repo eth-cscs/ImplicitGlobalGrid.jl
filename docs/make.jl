0f65105dbc18006c11ec2482828020c690b6e9c5
Samuel Omlin <samuel.omlin@cscs.ch>
2022-08-11 18:11:26 +0200

    copy examples to assets

--- docs/make.jl
+++ docs/make.jl
@@ -4,9 +4,15 @@ using DocExtensions
 using DocExtensions.DocumenterExtensions
 
 const DOCSRC      = joinpath(@__DIR__, "src")
+const DOCASSETS   = joinpath(DOCSRC, "assets")
 const EXAMPLEROOT = joinpath(@__DIR__, "..", "examples")
 
 DocMeta.setdocmeta!(ImplicitGlobalGrid, :DocTestSetup, :(using ImplicitGlobalGrid); recursive=true)
+
+
+@info "Copy examples folder to assets..."
+mkpath(DOCASSETS)
+cp(EXAMPLEROOT, joinpath(DOCASSETS, "examples"); force=true)
 
 
 @info "Preprocessing .MD-files..."
@@ -21,13 +27,11 @@ makedocs(;
     repo     = "https://github.com/eth-cscs/ImplicitGlobalGrid.jl/blob/{commit}{path}#{line}",
     sitename = "ImplicitGlobalGrid.jl",
     format   = Documenter.HTML(;
-        prettyurls       = get(ENV, "CI", "false") == "true",
+        prettyurls       = true,
         canonical        = "https://omlins.github.io/ImplicitGlobalGrid.jl",
         collapselevel    = 1,
         sidebar_sitename = true,
         edit_link        = "master",
-        #assets           = [asset("https://img.shields.io/github/stars/eth-cscs/ImplicitGlobalGrid.jl.svg", class = :ico)],
-        #warn_outdated    = true,
     ),
     pages   = [
         "Introduction"  => "index.md",
