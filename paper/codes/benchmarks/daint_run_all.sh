4045ff273ce22045eec9d21226063d77609905f5
Samuel Omlin <samuel.omlin@cscs.ch>
2022-09-30 13:07:25 +0200

    add paper folder

---
+++ paper/codes/benchmarks/daint_run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sbatch daint_runme_1.sh
+sbatch daint_runme_8.sh
+sbatch daint_runme_27.sh
+sbatch daint_runme_64.sh
+sbatch daint_runme_125.sh
+sbatch daint_runme_216.sh
+sbatch daint_runme_512.sh
+sbatch daint_runme_1000.sh
+sbatch daint_runme_2197.sh
---
+++ paper/codes/benchmarks/daint_runme_64.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+#SBATCH --job-name="diff3D_64"
+#SBATCH --output=diff3D_64.%j.o
+#SBATCH --error=diff3D_64.%j.e
+#SBATCH --time=00:30:00
+#SBATCH --nodes=64
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=normal
+#SBATCH --constraint=gpu
+#SBATCH --account c23
+
+module load daint-gpu
+module load Julia/1.7.2-CrayGNU-21.09-cuda
+module load cray-hdf5-parallel
+
+export JULIA_HDF5_PATH=$HDF5_ROOT
+export JULIA_CUDA_MEMORY_POOL=none
+
+export IGG_CUDAAWARE_MPI=1
+export MPICH_RDMA_ENABLED_CUDA=1
+
+scp diff3D.jl daint_submit_pareff.sh $SCRATCH/diff3D
+
+pushd $SCRATCH/diff3D
+
+chmod +x *.sh
+
+srun daint_submit_pareff.sh
+
+scp out_diff3D_pareff* $HOME/diff3D
