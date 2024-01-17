5cc267e6795d303bfe7412403cd3c19682285568
Samuel Omlin <samuel.omlin@cscs.ch>
2024-01-17 17:19:41 +0100

    include shared.jl in extension module

--- ext/ImplicitGlobalGrid_AMDGPUExt.jl
+++ ext/ImplicitGlobalGrid_AMDGPUExt.jl
@@ -1,3 +1,4 @@
 module ImplicitGlobalGrid_AMDGPUExt
+    include(joinpath(@__DIR__, "..", "src", "AMDGPUExt", "shared.jl"))
     include(joinpath(@__DIR__, "..", "src", "AMDGPUExt", "update_halo.jl"))
 end
--- ext/ImplicitGlobalGrid_CUDAExt.jl
+++ ext/ImplicitGlobalGrid_CUDAExt.jl
@@ -1,3 +1,4 @@
 module ImplicitGlobalGrid_CUDAExt
+    include(joinpath(@__DIR__, "..", "src", "CUDAExt", "shared.jl"))
     include(joinpath(@__DIR__, "..", "src", "CUDAExt", "update_halo.jl"))
 end
