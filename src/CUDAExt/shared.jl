6834dceff7f0bb9591a43c578951c7646c94e8c2
Samuel Omlin <samuel.omlin@cscs.ch>
2024-12-20 12:38:01 +0100

    widen array support to any bitstype dense array (CPU, CUDA, AMDGPU), including contiguous SubArrays

--- src/CUDAExt/shared.jl
+++ src/CUDAExt/shared.jl
@@ -1,13 +1,14 @@
 import ImplicitGlobalGrid
 import ImplicitGlobalGrid: GGArray, GGField, GGNumber, halosize, ol, cudaaware_MPI, sendranges, recvranges, sendbuf_flat, recvbuf_flat, write_d2x!, read_x2d!, write_d2h_async!, read_h2d_async!, register, is_cuarray
 import ImplicitGlobalGrid: NNEIGHBORS_PER_DIM, GG_ALLOC_GRANULARITY
+import LinearAlgebra
 using CUDA
 
 
 ##------
 ## TYPES
 
-const CuField{T,N} = GGField{T,N,CuArray{T,N}}
+const CuField{T,N} = GGField{T,N,<:AnyCuArray{T,N}}
 
 
 ##------------------------------------
@@ -20,15 +21,12 @@ ImplicitGlobalGrid.is_functional(::Val{:CUDA})                   = CUDA.function
 ##-------------
 ## SYNTAX SUGAR
 
-ImplicitGlobalGrid.is_cuarray(A::CuArray) = true   #NOTE: this function is only to be used when multiple dispatch on the type of the array seems an overkill (in particular when only something needs to be done for the GPU case, but nothing for the CPU case) and as long as performance does not suffer.
+ImplicitGlobalGrid.is_cuarray(A::AnyCuArray) = true   #NOTE: this function is only to be used when multiple dispatch on the type of the array seems an overkill (in particular when only something needs to be done for the GPU case, but nothing for the CPU case) and as long as performance does not suffer.
 
 
 ##--------------------------------------------------------------------------------
 ## FUNCTIONS FOR WRAPPING ARRAYS AND FIELDS AND DEFINE ARRAY PROPERTY BASE METHODS
 
-ImplicitGlobalGrid.wrap_field(A::CuArray, hw::Tuple) = CuField{eltype(A), ndims(A)}((A, hw))
-
-Base.size(A::CuField)          = Base.size(A.A)
 Base.size(A::CuField, args...) = Base.size(A.A, args...)
 Base.length(A::CuField)        = Base.length(A.A)
 Base.ndims(A::CuField)         = Base.ndims(A.A)
