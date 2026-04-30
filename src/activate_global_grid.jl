7510f43652ee76b0c68f804775a21e550f0d6302
Dvegrod <44696967+Dvegrod@users.noreply.github.com>
2026-04-30 11:26:12 +0200

    Update src/activate_global_grid.jl
    Co-authored-by: Samuel Omlin <samuel.omlin@cscs.ch>

--- src/activate_global_grid.jl
+++ src/activate_global_grid.jl
@@ -12,7 +12,7 @@ Replaces the current active grid parameters with the ones provided by `new_gg`. 
 # Usage example
     Given two local domains of different size and/or ghost cell properties: we have array `A1` and `B1` on grid `gg1`, and array `A2`, `B2` on grid `gg2`.
     
-    activate_global_grid(A)       # Activate the first grid configuration
+    activate_global_grid(gg1)       # Activate the first grid configuration
     update_halo!(A1, B1)  # Update the halo regions of arrays A1 and B1 on grid gg1
     activate_global_grid(gg2)       # Activate the second grid configuration
     update_halo!(smaller_array_B) # Update the halo regions of smaller_array_B according to configuration B
