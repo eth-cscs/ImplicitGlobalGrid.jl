a1e6c498552fd5b8d7ffbfa275d57e5ec2433fca
Dvegrod <44696967+Dvegrod@users.noreply.github.com>
2026-04-30 11:03:34 +0200

    Update src/update_halo.jl
    Co-authored-by: Samuel Omlin <samuel.omlin@cscs.ch>

--- src/update_halo.jl
+++ src/update_halo.jl
@@ -8,7 +8,7 @@ export update_halo!
 !!! note "Advanced"
         update_halo!(A, B, (A=C, halowidths=..., (A=D, halowidths=...), ...)
 
-Update the halo of the given GPU/CPU-array(s) following the currently active global grid specification or an explicitly passed global grid. Optionally a global grid specification can be passed, overriding the currently active grid.
+Update the halo of the given GPU/CPU-array(s). If multiple global grades have been defined, the currently active global grid will be used. Optionally a global grid specification can be passed, overriding the currently active grid.
 
 # Typical use cases:
     update_halo!(A)                                # Update the halo of the array A.
